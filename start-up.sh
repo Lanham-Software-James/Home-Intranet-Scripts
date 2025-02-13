61e051440bbac2bd1856a55b6efa46a81cfb8b9a
James Lanham <james.r.lanham2021@gmail.com>
2025-02-13 16:49:12 -0500

    Adding start all services script

---
+++ start-up.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cd /opt/stacks/pi-hole
+docker compose up -d
+
+cd /opt/stacks/homepage
+docker compose up -d
+
+cd /opt/stacks/jenkins
+docker compose up -d
+
+#Other services here
+
+cd /opt/stacks/nginx
+docker compose up -d
